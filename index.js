ae264787147292f04bf03c3f2510a1eca5c3e96c
Alexander Keith Wilson <fonzmp@gmail.com>
2018-04-17 21:39:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(arrayMusic, arrayArtist){
   let newArray = [];
   for(let i = 0; i < arrayMusic.length; i++){
-    newArray.push(`${arrayArtist[i]} plays ${arrayMusic[i]}`);
+    newArray.push(`${arrayMusic[i]} plays ${arrayArtist[i]}`);
   }
   return newArray;
 }
