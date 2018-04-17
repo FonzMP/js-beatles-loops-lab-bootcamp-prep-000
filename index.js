053aed9eb187792b234883e07ed2c008aac66523
Alexander Keith Wilson <fonzmp@gmail.com>
2018-04-17 21:51:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(arrayMusic, arrayArtist){
 function johnLennonFacts(array){
   let length = array.length;
   while(length > 0){
-    console.log(array[i]);
+    console.log(array[length]);
     length--;
   }
 }
